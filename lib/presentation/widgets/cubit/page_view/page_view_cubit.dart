cb3b41c44ab7c77d9f344bb217d878509ad32bdc
Chau Dinh Thong <dinhthongchau@gmail.com>
2025-05-19 14:24:33 +0700

    Update: delete Vietnamese comments

--- lib/presentation/widgets/cubit/page_view/page_view_cubit.dart
+++ lib/presentation/widgets/cubit/page_view/page_view_cubit.dart
@@ -1,4 +1,3 @@
-
 import 'package:finance_management/presentation/widgets/cubit/bottom_navigation/bottom_navigation_cubit.dart';
 import 'package:finance_management/presentation/widgets/cubit/page_view/page_view_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,7 +8,6 @@ class PageViewCubit extends Cubit<PageViewState> {
 
   PageViewCubit({required this.bottomNavigationCubit})
     : super(PageViewState(currentPage: 0, controller: PageController())) {
-    // Lắng nghe sự thay đổi từ BottomNavigationCubit
     bottomNavigationCubit.stream.listen((navState) {
       if (navState.currentIndex != state.currentPage) {
         animateToPage(navState.currentIndex);
@@ -17,7 +15,6 @@ class PageViewCubit extends Cubit<PageViewState> {
     });
   }
 
-  // Chuyển trang với animation
   void animateToPage(int page) {
     state.controller.animateToPage(
       page,
@@ -27,7 +24,6 @@ class PageViewCubit extends Cubit<PageViewState> {
     emit(state.copyWith(currentPage: page));
   }
 
-  // Xử lý khi người dùng vuốt để chuyển trang
   void onPageChanged(int page) {
     emit(state.copyWith(currentPage: page));
     bottomNavigationCubit.setTab(page);
